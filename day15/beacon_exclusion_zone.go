b9a9c373abe00cdc48ce132a5a85768e5be2d64c
Michal Huras <el88mike@gmail.com>
2022-12-16 17:19:47 +0100

    feat: day15, adding concurrency

--- day15/beacon_exclusion_zone.go
+++ day15/beacon_exclusion_zone.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"sort"
+	"sync"
 	"time"
 )
 
@@ -13,8 +14,111 @@ const (
 	TestInputFilename = "test_input.txt"
 )
 
-func overlap(source, target []int) bool {
-	return source[0] <= target[1] && source[1] >= target[0]
+const (
+	SearchSize = 4000000
+	Multiplier = 4000000
+)
+
+// NumberOfBatches - after some testing, seems like around 10 goroutines calculate
+// the result fastest.
+const NumberOfBatches = 10
+
+func BeaconExclusionZone() {
+	start := time.Now()
+
+	scanner, err := common.GetFileScanner("./day15/" + InputFilename)
+	if err != nil {
+		panic(err)
+	}
+
+	var sensors [][]int
+	var distances []int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		if line == "" {
+			continue
+		}
+
+		numbers, err := common.GetNumbersFromLine(line)
+		if err != nil {
+			panic(err)
+		}
+
+		sensor := []int{numbers[0], numbers[1]}
+		beacon := []int{numbers[2], numbers[3]}
+		distance := getDistance(sensor, beacon)
+
+		sensors = append(sensors, sensor)
+		distances = append(distances, distance)
+	}
+
+	targetX, targetY := 0, 0
+
+	wg := sync.WaitGroup{}
+	wg.Add(NumberOfBatches)
+
+	cancelCh := make(chan bool)
+
+	batchSize := SearchSize / NumberOfBatches
+
+	for n := 0; n < NumberOfBatches; n += 1 {
+		y := n * batchSize
+		yEnd := (y + batchSize) - 1
+
+		go func() {
+			for ; y <= yEnd; y += 1 {
+				select {
+				case <-cancelCh:
+					wg.Done()
+					return
+				default:
+					var ranges [][]int
+
+					for i, sensor := range sensors {
+						sy, d := sensor[1], distances[i]
+
+						upY := sy - d
+						downY := sy + d
+
+						diffX := 0
+
+						if y <= sy && upY <= y {
+							diffX = y - upY
+						} else if y > sy && downY >= y {
+							diffX = downY - y
+						} else {
+							continue
+						}
+
+						min := sensor[0] - diffX
+						max := sensor[0] + diffX
+
+						ranges = append(ranges, []int{min, max})
+					}
+
+					rangesOverlap, openRangeEndX := testRanges(ranges)
+
+					if !rangesOverlap {
+						cancelCh <- true
+						targetX, targetY = openRangeEndX+1, y
+					}
+				}
+			}
+
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	result := (targetX * Multiplier) + targetY
+
+	fmt.Println(result)
+
+	elapsed := time.Since(start)
+	log.Printf("Took %s", elapsed)
 }
 
 func testRanges(ranges [][]int) (bool, int) {
@@ -39,86 +143,6 @@ func testRanges(ranges [][]int) (bool, int) {
 	return true, -1
 }
 
-func BeaconExclusionZone() {
-	start := time.Now()
-
-	scanner, err := common.GetFileScanner("./day15/" + InputFilename)
-	if err != nil {
-		panic(err)
-	}
-
-	var sensors [][]int
-	var distances []int
-
-	target := 4000000
-	//target := 20
-
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		if line == "" {
-			continue
-		}
-
-		numbers, err := common.GetNumbersFromLine(line)
-		if err != nil {
-			panic(err)
-		}
-
-		sensor := []int{numbers[0], numbers[1]}
-		beacon := []int{numbers[2], numbers[3]}
-		distance := getDistance(sensor, beacon)
-
-		sensors = append(sensors, sensor)
-		distances = append(distances, distance)
-	}
-
-	x, y := 0, 0
-
-	var rangeMap [][][]int
-
-	for ; y <= target; y += 1 {
-		rangeMap = append(rangeMap, [][]int{})
-
-		for i, sensor := range sensors {
-			sy, d := sensor[1], distances[i]
-
-			upY := sy - d
-			downY := sy + d
-
-			diffX := 0
-
-			if y <= sy && upY <= y {
-				diffX = y - upY
-			} else if y > sy && downY >= y {
-				diffX = downY - y
-			} else {
-				continue
-			}
-
-			min := sensor[0] - diffX
-			max := sensor[0] + diffX
-
-			rangeMap[y] = append(rangeMap[y], []int{min, max})
-		}
-
-		rangesOverlap, openRangeEndX := testRanges(rangeMap[y])
-
-		if !rangesOverlap {
-			x = openRangeEndX + 1
-			break
-		}
-	}
-
-	multiplier := 4000000
-	result := (x * multiplier) + y
-
-	fmt.Println(result)
-
-	elapsed := time.Since(start)
-	log.Printf("Took %s", elapsed)
-}
-
 func getDistance(sensor, beacon []int) int {
 	sx, sy, bx, by := sensor[0], sensor[1], beacon[0], beacon[1]
 
@@ -138,4 +162,8 @@ func getDistance(sensor, beacon []int) int {
 	}
 
 	return dx + dy
+}
+
+func overlap(source, target []int) bool {
+	return source[0] <= target[1] && source[1] >= target[0]
 }
